c3af85bc482d693df0e0ab33580ebe94350c9249
Maheshkadam-Delxn <maheshkadam.delxn@gmail.com>
2025-04-09 11:35:19 +0530

    :change

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="scrollbar scrollbar-none bg-black">
       <HeroSection />
-      <Why_Choose />
+      {/* <Why_Choose /> */}
       <Choose/>
       {/* <Demo /> */}
       <Services />
